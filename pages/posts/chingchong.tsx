13af28f617f05c89f53210d98397f84ce23a9172
Trần Mạnh Hùng (VINID-KCN-HN) <hungtm21@vingroup.net>
2020-09-15 14:11:25 +0700

    fix bug

--- pages/posts/chingchong.tsx
+++ pages/posts/chingchong.tsx
@@ -1,50 +1,10 @@
 
 
-import { GetStaticProps, InferGetServerSidePropsType } from 'next'
 import React from 'react'
-import { useSelector } from 'react-redux'
-import fs from 'fs'
-import path from 'path'
 import ActiveLink from 'pages/components/ActiveLink'
-import { useRouter } from 'next/router'
-
-interface Post {
-    name: string
-}
 
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    // const res = await fetch('https://.../posts')
-    const posts: Post[] = [{ name: "wala" }]
-
-    return {
-        props: {
-            posts,
-        },
-        revalidate: 1
-    }
-}
-
-export async function getStaticPaths() {
-    // Call an external API endpoint to get posts
-    // const res = await fetch('https://.../posts')
-    // const posts = await res.json()
-    const posts = [{ id: 1 }, { id: 2 }]
-    // Get the paths we want to pre-render based on posts
-    const paths = posts.map((post) => ({
-        params: { id: post.id },
-    }))
-
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
-    return { paths, fallback: false }
-}
-
-const ChingChong = ({ posts }: InferGetServerSidePropsType<typeof getStaticProps>) => {
-    console.log(posts)
-    const router = useRouter()
-    const params = router.query
-    console.log(params)
+const ChingChong = () => {
     return (
         <div>
             <ul>
